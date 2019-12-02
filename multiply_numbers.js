4742bef69c9f4afc1dec0f38b0be63498661cf51
rama8c6 <57110509+rama8c6@users.noreply.github.com>
2019-12-01 20:34:47 -0500

    Create multiply_numbers.js

---
+++ multiply_numbers.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+"use strict";
+
+function multiply() {
+    var x = Number(document.getElementById('x').value);
+    var y = Number(document.getElementById('y').value);
+    var z = x * y;
+
+    document.getElementById('result').innerHTML = z;
+    return false;
+}
+
+document.getElementById('go').addEventListener('click', multiply);
